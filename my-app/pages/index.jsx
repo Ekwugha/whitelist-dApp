38cc30540004a0c0c5906d956dfe0874171f5c27
Ekwugha <elloekwugha@gmail.com>
2022-05-12 21:11:42 +0100

    changed network from rinkeby to goerli

--- my-app/pages/index.jsx
+++ my-app/pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Web3Modal from "web3modal";
-import { providers, Contract } from "ethers";;
+import { providers, Contract } from "ethers";
 import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../constants";
 import styles from '../styles/Home.module.css'
